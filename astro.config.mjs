e0c593a6881dd951fae1d147a18bbc5cdeb85f9a
Nafer Zamudio <naanzagi@gmail.com>
2026-04-27 22:32:11 -0500

    2.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://rmwiklist.netlify.app',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
