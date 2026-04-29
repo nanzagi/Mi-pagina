1a3b62cedfb2bf1f878e7e1b0dca1845bdbe3a2d
Nafer Zamudio <naanzagi@gmail.com>
2026-04-28 21:25:45 -0500

    n

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://rmwiklist.netlify.app',
+    site: 'https://rmwiklist.vercel.app',
     integrations: [mdx(), sitemap(), pagefind()],
     fonts: [
         {
